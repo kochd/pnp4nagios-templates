45346159b763049d369a6787d6e6aa8ac93caf32
Daniel Koch <dkoch@triple6.org>
2014-01-04 19:37:32 +0100

    check_ifstat: init

---
+++ check_ifstat.php
@@ -0,0 +1,17 @@
+<?php
+$ds_name[1] = "$NAGIOS_AUTH_SERVICEDESC"; 
+$opt[1] = "--vertical-label \"$UNIT[1]\" --title \"$hostname / $servicedesc\" ";
+$def[1]  = rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
+$def[1] .= rrd::def("var2", $RRDFILE[2], $DS[2], "AVERAGE");
+
+if ($WARN[1] != "") {
+    $def[1] .= "HRULE:$WARN[1]#FFFF00 ";
+}
+if ($CRIT[1] != "") {
+    $def[1] .= "HRULE:$CRIT[1]#FF0000 ";       
+}
+$def[1] .= rrd::line1("var1", "#0000ff", "$NAME[1]") ;
+$def[1] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[1] .= rrd::area("var2", "#ff0000", "$NAME[2]") ;
+$def[1] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+?>
