da3a3d12db7ca675e67ba665a43608b22728cc56
root <root@claw-storage.lan>
2014-01-04 13:40:40 +0100

    check_hddtemp: init

---
+++ check_hddtemp.php
@@ -0,0 +1,16 @@
+<?php
+$ds_name[1] = "$NAGIOS_AUTH_SERVICEDESC"; 
+$opt[1] = "--vertical-label \"$UNIT[1]\" --title \"$hostname / $servicedesc\" ";
+$def[1]  = rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
+
+if ($WARN[1] != "") {
+    $def[1] .= "HRULE:$WARN[1]#FFFF00 ";
+}
+if ($CRIT[1] != "") {
+    $def[1] .= "HRULE:$CRIT[1]#FF0000 ";       
+}
+
+$def[1] .= rrd::line3("var1", "#000000", "$NAME[1]") ;
+$def[1] .= rrd::gradient("var1", "#0000FF", "#FF0000", "$NAME[1]");
+$def[1] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+?>
