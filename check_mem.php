1e0a909b8a3916c8eb4da99812db1d89e2b89683
Daniel Koch <dkoch@triple6.org>
2014-01-07 22:25:39 +0100

    check_mem: added warn and crit line

--- check_mem.php
+++ check_mem.php
@@ -5,6 +5,13 @@ $opt[1] = "--vertical-label \"$UNIT[1]\" --title \"$hostname / $servicedesc / RA
 $def[1] = rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
 $def[1] .= rrd::def("var2", $RRDFILE[2], $DS[2], "AVERAGE");
 
+if ($WARN[1] != "") {
+    $def[1] .= "HRULE:$WARN[1]#FFFF00 ";
+}
+if ($CRIT[1] != "") {
+    $def[1] .= "HRULE:$CRIT[1]#FF0000 ";       
+}
+
 $def[1] .= rrd::area("var1", "#00ff00", "$NAME[1]") ;
 $def[1] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 $def[1] .= rrd::line3("var2", "#000000", "$NAME[2]") ;
