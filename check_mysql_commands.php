4624b7435dc65446ab549f6c7622a9d64b656825
Daniel Koch <koch@triple6.org>
2014-05-04 02:46:11 +0200

    Fixed check_mysql_commands template

--- check_mysql_commands.php
+++ check_mysql_commands.php
@@ -7,8 +7,8 @@ $def[1] .= rrd::def("var3", $RRDFILE[3], $DS[3], "AVERAGE");
 $def[1] .= rrd::def("var4", $RRDFILE[4], $DS[4], "AVERAGE");
 $def[1] .= rrd::def("var5", $RRDFILE[5], $DS[5], "AVERAGE");
 $def[1] .= rrd::def("var6", $RRDFILE[6], $DS[6], "AVERAGE");
-$def[1] .= rrd::def("var7", $RRDFILE[7], $DS[7], "AVERAGE");
-$def[1] .= rrd::def("var8", $RRDFILE[8], $DS[8], "AVERAGE");
+#$def[1] .= rrd::def("var7", $RRDFILE[7], $DS[7], "AVERAGE");
+#$def[1] .= rrd::def("var8", $RRDFILE[8], $DS[8], "AVERAGE");
 
 if ($WARN[1] != "") {
     $def[1] .= "HRULE:$WARN[1]#FFFF00 ";
@@ -18,8 +18,8 @@ if ($CRIT[1] != "") {
 }
 
 
-$def[1] .= rrd::area("var8", "#BBBBBB", "$NAME[8]") ;
-$def[1] .= rrd::gprint("var8", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[1] .= rrd::area("var6", "#BBBBBB", "$NAME[6]") ;
+$def[1] .= rrd::gprint("var6", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 $def[1] .= rrd::line2("var1", "#DF3030", "$NAME[1]") ;
 $def[1] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 $def[1] .= rrd::line2("var2", "#3038DF", "$NAME[2]") ;
@@ -30,10 +30,10 @@ $def[1] .= rrd::line2("var4", "#6F7C18", "$NAME[4]") ;
 $def[1] .= rrd::gprint("var4", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 $def[1] .= rrd::line2("var5", "#7C4418", "$NAME[5]") ;
 $def[1] .= rrd::gprint("var5", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var6", "#30187C", "$NAME[6]") ;
-$def[1] .= rrd::gprint("var6", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var7", "#B404AE", "$NAME[7]") ;
-$def[1] .= rrd::gprint("var7", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+#$def[1] .= rrd::line2("var6", "#30187C", "$NAME[6]") ;
+#$def[1] .= rrd::gprint("var6", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+#$def[1] .= rrd::line2("var7", "#B404AE", "$NAME[7]") ;
+#$def[1] .= rrd::gprint("var7", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 
 
 
